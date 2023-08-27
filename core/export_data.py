63e766966f66e941224a38912167ebfa1eef22c9
ryu-dayo <katerina1473@hotmail.com>
2023-08-27 17:09:42 +0800

    feat: 备份数据

---
+++ core/export_data.py
@@ -0,0 +1,26 @@
+import shutil
+import configparser
+from json import loads
+from function import file_path
+
+def export_data():
+    # 加载配置文件
+    config = configparser.ConfigParser()
+    config.read(file_path('config.ini'),encoding='UTF-8')
+
+    # 获得需要备份的文件和目标地址
+    file_name_list = loads(config['export']['file_list'])
+    target_folder = config['export']['path']
+
+    # 如果目标地址不为空则执行备份
+    if target_folder != '':
+
+        for file_name in file_name_list:
+            current_file = file_path(file_name)
+            target_file = target_folder + file_name
+            shutil.copy(current_file, target_file)
+
+        print(f'文件已成功备份至 {target_folder}')
+
+if __name__ == '__main__':
+    export_data()
--- core/main.py
+++ core/main.py
@@ -1,7 +1,9 @@
 from update_fund import update_fund
 from update_notion import update_notion
 from view_fund import view_fund
+from export_data import export_data
 
 update_fund()
 update_notion()
 view_fund()
+export_data()
