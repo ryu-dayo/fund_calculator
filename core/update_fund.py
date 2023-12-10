18be48d3e7aacad53868dc225599679e89232b75
ryu-dayo <katerina1473@hotmail.com>
2023-12-10 14:18:20 +0800

    style

--- core/update_fund.py
+++ core/update_fund.py
@@ -15,63 +15,60 @@ def trans_filter(my_trans,fundcode,parent_type,info=['Buy','Sell','Income'],star
     info: 交易类型，列表类型
     star_index: 起始索引
     '''
-    trans_info = my_trans[
-        (my_trans['fundcode']==fundcode)&
-        (my_trans['parent']==parent_type)&
-        (my_trans['info'].isin(info))&
-        (my_trans.index>=star_index)
-    ]
-    return trans_info
+    return my_trans.query(
+        "fundcode == @fundcode and parent == @parent_type and "
+        "info in @info and index >= @star_index"
+    )
 
 def if_liquidation(my_trans,fundcode,parent_type):
     '判断是否有过清仓，返回最后一次清仓交易的行索引+1'
     fund_trans = trans_filter(my_trans,fundcode,parent_type).copy()
     fund_trans['remaining_shares'] = np.cumsum(fund_trans['number_shares']).round(2)
-    if 0.00 in list(fund_trans['remaining_shares']):
-        star_index = fund_trans[fund_trans['remaining_shares']==0].index.values[-1]+1
+
+    liquidation_rows = fund_trans['remaining_shares'] == 0
+    if not fund_trans[liquidation_rows].empty:
+        star_index = fund_trans[liquidation_rows].index.values[-1] + 1
     else:
         star_index = 0
+
     return star_index
 
 def calculate_fund_data(my_trans,fundcode,parent_type,star_index):
-    '计算基金各项数据'
-    trans_sell_df = trans_filter(my_trans,fundcode,parent_type,['Sell'],star_index)
-    trans_income_df = trans_filter(my_trans,fundcode,parent_type,['Income'],star_index)
-    trans_buy_df = trans_filter(my_trans,fundcode,parent_type,['Buy'],star_index)
-    trans_buy_all = trans_filter(my_trans,fundcode,parent_type,['Buy'])
-    trans_income_all = trans_filter(my_trans,fundcode,parent_type,['Income'])
-    trans_sell_all = trans_filter(my_trans,fundcode,parent_type,['Sell'])
+    # 获取包含所有相关交易类型的完整数据集（从star_index开始）
+    trans_liquidation_df = trans_filter(my_trans,fundcode,parent_type,star_index=star_index)
+
+    trans_buy_df = trans_liquidation_df[trans_liquidation_df['info'] == 'Buy']
+    trans_sell_df = trans_liquidation_df[trans_liquidation_df['info'] == 'Sell']
+    trans_income_df = trans_liquidation_df[trans_liquidation_df['info'] == 'Income']
+    
+    # 获取包含所有相关交易类型的完整数据集（不考虑star_index）
     trans_all_df = trans_filter(my_trans,fundcode,parent_type)
+
+    trans_buy_all = trans_all_df[trans_all_df['info'] == 'Buy']
+    trans_income_all = trans_all_df[trans_all_df['info'] == 'Income']
+    trans_sell_all = trans_all_df[trans_all_df['info'] == 'Sell']
     
     sell_shares = trans_sell_df['number_shares'].sum()
     jjfh = round(trans_income_df['total_price'].sum(),2)
     cyfe = round(
-        trans_buy_df['number_shares'].sum()+
-        trans_income_df['number_shares'].sum()+
-        sell_shares,
+        trans_buy_df['number_shares'].sum()
+        + trans_income_df['number_shares'].sum()
+        + sell_shares,
         2
     )
-    jjbj = round(
-        trans_buy_df['total_price'].sum()+
-        trans_buy_df['commission'].sum(),
-        2
-    )
+    jjbj = round(trans_buy_df[['total_price','commission']].sum().sum(),2)
     if cyfe > 0:
         cccb = jjbj/(cyfe-sell_shares)
     else:
         cccb = 0
     lj_cyfe = round(
-        trans_buy_all['number_shares'].sum()+
-        trans_income_all['number_shares'].sum()+
-        trans_sell_all['number_shares'].sum(),
+        trans_buy_all['number_shares'].sum()
+        + trans_income_all['number_shares'].sum()
+        + trans_sell_all['number_shares'].sum(),
         2
     )
     lj_mcje = trans_sell_all["total_price"].sum()
-    lj_jjbj = round(
-        trans_buy_all['total_price'].sum()+
-        trans_buy_all['commission'].sum(),
-        2
-    )
+    lj_jjbj = round(trans_buy_all[['total_price','commission']].sum().sum(),2)
     lj_jjfh = round(trans_income_all['total_price'].sum(),2)
     child = trans_all_df.loc[trans_all_df.index[-1],'child']
     data_info = [fundcode,cyfe,parent_type,child,jjfh,jjbj,cccb,lj_cyfe,lj_jjfh,lj_jjbj,lj_mcje]
@@ -101,15 +98,18 @@ def calculate_base_data(my_trans):
 
 def merge_data(my_fund,trans_data):
     '组装数据'
-    for repeat_columns in ['cyfe','child','jjfh','jjbj','cccb','lj_cyfe','lj_jjfh','lj_jjbj','lj_mcje']:
-        if repeat_columns in my_fund.columns:
-            my_fund = my_fund.drop(repeat_columns,axis=1)
+    columns_to_drop = [
+        'cyfe','child','jjfh','jjbj','cccb','lj_cyfe','lj_jjfh','lj_jjbj','lj_mcje'
+    ]
+
+    my_fund_cleaned = my_fund.drop(columns=columns_to_drop,errors='ignore')
     my_fund_data = pd.merge(
-        my_fund,
+        my_fund_cleaned,
         trans_data,
         on=['fundcode','parent'],
         how='right'
     )
+
     return my_fund_data
 
 def add_fund_name(my_fund_data):
@@ -123,43 +123,51 @@ def add_fund_name(my_fund_data):
             name_dict = name_df.set_index('fundcode').T.to_dict('list')
         except:
             name_dict = get_name_info()
-        my_fund_data['name'] = my_fund_data['fundcode'].apply(lambda x:name_dict.get(x)[0])
-        my_fund_data['type'] = my_fund_data['fundcode'].apply(lambda x:name_dict.get(x)[1])
+
+        my_fund_data['name'] = my_fund_data['fundcode'].map(lambda x: name_dict.get(x,[None,None])[0])
+        my_fund_data['type'] = my_fund_data['fundcode'].map(lambda x: name_dict.get(x,[None,None])[1])
 
 def update_fund_nav(my_fund_data):
     '若净值更新日期不是今天，则更新净值'
     date_today = datetime.now().strftime('%Y-%m-%d')
-    # date_today = '2023-02-03' #测试用
-    fund_list = my_fund_data[
-        (my_fund_data['FSRQ']!=date_today)&(my_fund_data['cyfe']>0)
-    ]['fundcode'].unique()
-    if len(fund_list)>0:
+    
+    fund_need_update = my_fund_data[
+        (my_fund_data['FSRQ'] != date_today) & 
+        (my_fund_data['cyfe'] > 0)
+    ]
+
+    fund_list = fund_need_update['fundcode'].unique()
+    
+    if len(fund_list) > 0:
         dwjz_dict = get_nav(fund_list)
-        not_today_index = my_fund_data[
-            (my_fund_data['FSRQ']!=date_today)&(my_fund_data['cyfe']>0)
-        ].index
-        my_fund_data.loc[not_today_index,'DWJZ'] = my_fund_data.loc[
-            not_today_index,'fundcode'
-        ].apply(lambda x:dwjz_dict.get(x)[1]).astype(float)
-        my_fund_data.loc[not_today_index,'FSRQ'] = my_fund_data.loc[
-            not_today_index,'fundcode'
-        ].apply(lambda x:dwjz_dict.get(x)[0])
+        not_today_index = fund_need_update.index
+        dwjz_values = my_fund_data.loc[not_today_index,'fundcode'].map(dwjz_dict)
+
+        my_fund_data.loc[not_today_index,'DWJZ'] = dwjz_values.str[1].astype(float)
+        my_fund_data.loc[not_today_index,'FSRQ'] = dwjz_values.str[0]
 
     # 处理为空的净值，一般为新录入且已清仓的基金
     my_fund_data['DWJZ'] = my_fund_data['DWJZ'].fillna(0)
 
 def calculate_revenue(my_fund_data):
     '计算累计收益，持有收益，持有收益率，持有市值'
-    my_fund_data["ljsy"] = (my_fund_data["lj_cyfe"]*my_fund_data["DWJZ"]+my_fund_data["lj_mcje"]-my_fund_data["lj_jjbj"]+my_fund_data["lj_jjfh"]).round(2)
+    my_fund_data["ljsy"] = (
+        my_fund_data["lj_cyfe"] * my_fund_data["DWJZ"]
+        + my_fund_data["lj_mcje"]
+        - my_fund_data["lj_jjbj"]
+        + my_fund_data["lj_jjfh"]
+    ).round(2)
     
-    my_fund_data[["cysy","cysyl"]] = 0.0
-    my_fund_data.loc[my_fund_data["cyfe"]>0,"cysy"] = (my_fund_data["DWJZ"]-my_fund_data["cccb"])*my_fund_data["cyfe"]+my_fund_data["jjfh"]
-    my_fund_data.loc[my_fund_data["cyfe"]>0,"cysyl"] = (my_fund_data["cysy"]/(my_fund_data["cccb"]*my_fund_data['cyfe'])).round(4)
+    condition = my_fund_data["cyfe"] > 0
+
+    cysy_calculate = (my_fund_data["DWJZ"] - my_fund_data["cccb"]) * my_fund_data["cyfe"] + my_fund_data["jjfh"]
+    cysyl_calculate = cysy_calculate / (my_fund_data["cccb"] * my_fund_data['cyfe'])
+
+    my_fund_data['cysy'] = np.where(condition,cysy_calculate.round(2),0)
+    my_fund_data['cysyl'] = np.where(condition,cysyl_calculate.round(4),0)
 
     my_fund_data['cccb'] = my_fund_data['cccb'].round(4)
-    my_fund_data['cysy'] = my_fund_data['cysy'].round(2)
-
-    my_fund_data['amount'] = (my_fund_data['cyfe']*my_fund_data['DWJZ']).round(2) # 计算每个基金的总金额
+    my_fund_data['amount'] = (my_fund_data['cyfe'] * my_fund_data['DWJZ']).round(2) # 计算每个基金的总金额
     # print(my_fund_data)
 
 def update_fund():
@@ -182,9 +190,7 @@ def update_fund():
         )
 
     # 处理数据，将卖出的份额变为负数
-    my_trans.loc[(my_trans['info']=='Sell'),'number_shares'] = -my_trans.loc[
-        (my_trans['info']=='Sell'),'number_shares'
-    ]
+    my_trans.loc[my_trans['info']=='Sell','number_shares'] *= -1
 
     trans_data = calculate_base_data(my_trans)
     my_fund_data = merge_data(my_fund,trans_data)
